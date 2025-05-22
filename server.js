f6c109091dcd56637e005570302b0fb6986ec3a8
QuickEAM <leiene.neves@gmail.com>
2025-05-22 14:24:44 -0300

    Corrige porta para Render

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
-
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
+const path = require('path');
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
